9ea4d459524f4067f6d31906cc337a8ef7037638
Jeff Ruble <jeff.ruble@gmail.com>
2021-06-25 10:46:16 -0400

    Adds array of objects containing color theme information. Adds function to toggle light and dark themes

--- app.js
+++ app.js
@@ -4,9 +4,43 @@
 // If no preexisting notes, sets userNotes to empty array
 let userNotes = JSON.parse(localStorage.getItem('notes')) || [];
 
+const colorTheme = [
+
+    {
+        themeName: 'Light',
+        mainTextColor: 'rgb(0, 0, 0)',
+        mainBackgroundColor: 'rgb(255, 255, 255)',
+        mainNotesColor: 'rgb(211, 211, 211)',
+    },
+    {
+        themeName: 'Dark',
+        mainTextColor: 'rgb(255, 255, 255)',
+        mainBackgroundColor: 'rgb(0, 0, 0)',
+        mainNotesColor: 'rgb(75, 75, 75)',    
+    }
+
+];
+
 const newNoteButton = document.querySelector('nav ul li');
+const themeControl = document.getElementById('theme-control');
 const closeModalButton = document.querySelector('#modal-footer li');
 const textArea = document.querySelector('textarea');
+
+let root = document.documentElement;
+
+function changeColorTheme() {
+    if (this.textContent.includes(colorTheme[0].themeName)) {
+        root.style.setProperty('--main-text-color', colorTheme[1].mainTextColor);
+        root.style.setProperty('--main-background-color', colorTheme[1].mainBackgroundColor);
+        root.style.setProperty('--main-notes-color', colorTheme[1].mainNotesColor);
+        themeControl.textContent = `Theme: ${colorTheme[1].themeName}`;
+    } else {
+        root.style.setProperty('--main-text-color', colorTheme[0].mainTextColor);
+        root.style.setProperty('--main-background-color', colorTheme[0].mainBackgroundColor);
+        root.style.setProperty('--main-notes-color', colorTheme[0].mainNotesColor);
+        themeControl.textContent = `Theme: ${colorTheme[0].themeName}`;
+    }
+}
 
 function toggleModalDisplay() {
     
@@ -24,11 +58,6 @@ function countWords(text) {
     return text.match(/\w+/g).length;
 }
 
-function isBlank() {
-    const regex = new RegExp(/^(\s+$)(.{0})/);
-    return regex.test(note.text);
-}
-
 function addNote() {
 
     const text = textArea.value;
@@ -38,6 +67,11 @@ function addNote() {
         dateCreated,
         wordCount: countWords(textArea.value)
     }
+
+    function isBlank() {
+        const regex = new RegExp(/^(\s+$)(.{0})/);
+        return regex.test(note.text);
+    }    
 
     if (note.text !== '' && !isBlank()) {
         // Add note to top of notes array
@@ -77,4 +111,5 @@ textArea.addEventListener('input', () => {
     let wordCount = document.getElementById('wordcount');
     wordCount.innerHTML = 'Words:' + ' ' + countWords(textArea.value);
 });
-
+// Change color theme
+themeControl.addEventListener('click', changeColorTheme);
